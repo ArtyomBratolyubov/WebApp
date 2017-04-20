8c47a518e91977ae1dc60730729c1d1515e0694d
ARCHIE <ARCHIE@ARCHIE-PC>
2017-04-20 19:49:22 +0300

    Add project files.

---
+++ WebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApp
+{
+    public class BundleConfig
+    {
+
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                                  "~/Scripts/angular.js",
+                                  "~/Scripts/angular-route.js",
+                                  "~/Scripts/angular-touch.js",
+                                  "~/Scripts/angular-animate.js",
+                                  "~/Scripts/angular-ui/ui-bootstrap.js",
+                                  "~/js/main.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
